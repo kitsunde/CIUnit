c48c6e1f229d5132dc08e02df277d67b72b722e0
Clemens Gruenberger <clem@fragment.at>
2009-02-05 23:11:35 +0100

    enable multiple autoload includes during tests - added autoload method of CI_Loader to fooLoader

--- fooStack/fooLoader.php
+++ fooStack/fooLoader.php
@@ -244,6 +244,90 @@ class fooLoader extends CI_Loader {
 		$this->_ci_assign_to_models();
 	}
 
+    	// --------------------------------------------------------------------
+
+	/**
+	 * Autoloader
+	 *
+	 * The config/autoload.php file contains an array that permits sub-systems,
+	 * libraries, plugins, and helpers to be loaded automatically.
+	 *
+	 * @access	private
+	 * @param	array
+	 * @return	void
+	 */
+	function _ci_autoloader()
+	{
+        //enable multiple autoload during tests
+        include(APPPATH.'config/autoload'.EXT);
+		//include_once(APPPATH.'config/autoload'.EXT);
+
+		if ( ! isset($autoload))
+		{
+			return FALSE;
+		}
+
+		// Load any custom config file
+		if (count($autoload['config']) > 0)
+		{
+			$CI =& get_instance();
+			foreach ($autoload['config'] as $key => $val)
+			{
+				$CI->config->load($val);
+			}
+		}
+
+		// Autoload plugins, helpers and languages
+		foreach (array('helper', 'plugin', 'language') as $type)
+		{
+			if (isset($autoload[$type]) AND count($autoload[$type]) > 0)
+			{
+				$this->$type($autoload[$type]);
+			}
+		}
+
+		// A little tweak to remain backward compatible
+		// The $autoload['core'] item was deprecated
+		if ( ! isset($autoload['libraries']))
+		{
+			$autoload['libraries'] = $autoload['core'];
+		}
+
+		// Load libraries
+		if (isset($autoload['libraries']) AND count($autoload['libraries']) > 0)
+		{
+			// Load the database driver.
+			if (in_array('database', $autoload['libraries']))
+			{
+				$this->database();
+				$autoload['libraries'] = array_diff($autoload['libraries'], array('database'));
+			}
+
+			// Load scaffolding
+			if (in_array('scaffolding', $autoload['libraries']))
+			{
+				$this->scaffolding();
+				$autoload['libraries'] = array_diff($autoload['libraries'], array('scaffolding'));
+			}
+
+			// Load all other libraries
+			foreach ($autoload['libraries'] as $item)
+			{
+				$this->library($item);
+			}
+		}
+
+		// Autoload models
+		if (isset($autoload['model']))
+		{
+			$this->model($autoload['model']);
+		}
+
+	}
+
+	// --------------------------------------------------------------------
+
+
 }
 
 ?>
