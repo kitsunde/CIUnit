9ab62ba5d2c15a229bee9b0609f2ed1f1c9ce650
Clemens Gruenberger <clem@fragment.at>
2009-02-12 23:10:32 +0100

    fixed static files function in CIUnit class, formatting improved

--- tests/CIUnit.php
+++ tests/CIUnit.php
@@ -14,13 +14,14 @@ error_reporting(E_STRICT);
 require_once 'PHPUnit/Framework.php';
 require_once 'CIUnitTestCase.php';
 require_once 'CIUnitTestSuite.php';
-define('CIUnit_Version', '0.10');
+
+define('CIUnit_Version', '0.14');
 
 //start CodeIgniter
-$rel_dir = dirname(__FILE__).'/../../../';
-include_once $rel_dir.'index.php';
+$rel_dir = dirname(__FILE__) . '/../../../';
+include_once $rel_dir . 'index.php';
 
-define('TESTSPATH', APPPATH.'tests/');
+define('TESTSPATH', APPPATH . 'tests/');
 
 /**
 * CIUnit Class
@@ -43,7 +44,7 @@ define('TESTSPATH', APPPATH.'tests/');
 * $vars = viewvars();
 * they are also reset after this call
 */
-class CIUnit{
+class CIUnit {
 
     private static $instance;
 
@@ -54,19 +55,23 @@ class CIUnit{
     public static $spyc;
     public static $fixture;
 
-    public function __construct(){
+    public function __construct()
+    {
         self::$instance = &$this;
     }
 
-    public function &get_CIU(){
+    public function &get_CIU()
+    {
         return self::$instance;
     }
 
-    public static function &set_controller($controller='Controller'){
+    public static function &set_controller($controller='Controller')
+    {
         $controller_name = array_pop(split('/', $controller));
         //echo "\nc name ".$controller_name;
         //is it the current controller?
-        if($controller_name == self::$current){
+        if ($controller_name == self::$current)
+        {
             //we have nothing to do, return current controller
             //echo "current found!";
             output(); viewvars();
@@ -74,12 +79,12 @@ class CIUnit{
         }
 
         //the current controller must be archieved before littered
-        $loader =& load_class('Loader');
+        $loader = &load_class('Loader');
         self::$controllers[self::$current] = array(
             'adress' => self::$controller,
             'models' => $loader->_ci_models,  //this might be an update if it was there before
-           // FIXME, all additional properties of the loader / controllers
-          // that have to be reset must go in some test config file..
+            // FIXME, all additional properties of the loader / controllers
+            // that have to be reset must go in some test config file..
             //'components' => $loader->_ci_components,
             //'classes' => $loader->_ci_classes
             );
@@ -91,11 +96,13 @@ class CIUnit{
         //reset saved queries
         self::$controller->db->queries = array();
         //clean output / viewvars as well;
-        if(isset(self::$controller->output)){
+        if ( isset(self::$controller->output) )
+        {
             output(); viewvars();
         }
         //the requested controller was loaded before?
-        if(isset(self::$controllers[$controller_name])){
+        if ( isset(self::$controllers[$controller_name]) )
+        {
             //echo "saved found!";
             //load it
             $old = self::$controllers[$controller_name];
@@ -105,11 +112,14 @@ class CIUnit{
             //$loader->_ci_components = $old['components'];
             //$loader->_ci_classes = $old['classes'];
             CI_Base::$instance = self::$controller; //so get_instance() provides the correct controller
-        }else{
+        }
+        else
+        {
             //echo "load new";
             //it was not loaded before
-            if(!class_exists($controller_name)){
-                include_once(APPPATH.'controllers/'.$controller.EXT);
+            if (!class_exists($controller_name))
+            {
+                include_once(APPPATH . 'controllers/' . $controller . EXT);
             }
             self::$controller = new $controller_name();
             self::$current = $controller_name;
@@ -118,32 +128,38 @@ class CIUnit{
     }
 
 
-    public static function &get_controller(){
+    public static function &get_controller()
+    {
         return self::$controller;
     }
 
     /**
     * get filenames eg for running test suites
+    * $path is relative
     */
-    public static function files($pattern, $path=".", $addpath=FALSE){ // lists all pdf files as links
-        if (strpos($path, '/') === FALSE){
-          if (function_exists('realpath') AND @realpath(dirname(__FILE__)) !== FALSE){
-            $system_folder = realpath(dirname(__FILE__)).'/'.$path;
-          }
-        }
-        else{
-          // Swap directory separators to Unix style for consistency
-          $path = str_replace("\\", "/", $system_folder);
-        }
-        if(substr($path,-1)!="/"){$path.="/";}
-        $dir_handle = @opendir($path) or die("Unable to open $path");
-        $outarr=array();
+    public static function files($pattern, $path = ".", $addpath = FALSE)
+    {
+        // Swap directory separators to Unix style for consistency
+        $path = str_replace("\\", "/", $path);
 
-        while (false !== ($file = readdir($dir_handle)) ){
-          if (preg_match($pattern, $file)){
-            if($addpath){$file=$path.$file;}
-            $outarr[]=$file;
-          }
+        if (substr($path, -1) !== "/")
+        {
+            $path .= "/";
+        }
+
+        $dir_handle = @opendir($path) or die("Unable to open $path");
+        $outarr = array();
+
+        while ( false != ($file = readdir($dir_handle)) )
+        {
+            if (preg_match($pattern, $file))
+            {
+                if ($addpath)
+                {
+                    $file = $path . $file;
+                }
+                $outarr[] = $file;
+            }
         }
         //could also use preg_grep!
         closedir($dir_handle);
@@ -157,7 +173,8 @@ class CIUnit{
 /**
 * retrieves current CIUnit Class Singleton
 */
-function &get_CIU(){
+function &get_CIU()
+{
     return CIUnit::get_CIU();
 }
 
@@ -174,28 +191,31 @@ function &set_controller($controller='Controller')
 */
 function &get_controller()
 {
-  return CIUnit::get_controller();
+    return CIUnit::get_controller();
 }
 
 /**
 * retrieves the cached output from the output class
 * and resets it
 */
-function output(){
-  return CIUnit::$controller->output->pop_output();
+function output()
+{
+    return CIUnit::$controller->output->pop_output();
 }
 
 /**
 * retrieves the cached template vars from the loader class (stored here for assignment to views)
 * and resets them
 */
-function viewvars(){
-  if(isset(CIUnit::$controller->load->_ci_cached_vars)){
-    $out = CIUnit::$controller->load->_ci_cached_vars;
-    CIUnit::$controller->load->_ci_cached_vars = array();
-    return $out;
-  }
-  return array();
+function viewvars()
+{
+    if( isset(CIUnit::$controller->load->_ci_cached_vars) )
+    {
+        $out = CIUnit::$controller->load->_ci_cached_vars;
+        CIUnit::$controller->load->_ci_cached_vars = array();
+        return $out;
+    }
+    return array();
 }
 
 //=== and off we go ===
