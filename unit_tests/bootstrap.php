817ee9e4935bdb1838dba3b48dbc97122938ddf1
grisha <none@none>
2011-02-17 22:17:49 +0100

    Uploading latest version. Folder structure ready for CI 2.0 but the current code doesn't work since some things changed in CI core files. Working on that in free time.

---
+++ unit_tests/bootstrap.php
@@ -0,0 +1,8 @@
+<?php
+
+/**
+ * This file should contain pre test exectution preparations
+ */
+
+require_once 'PHPUnit/TextUI/TestRunner.php';
+require_once (realpath(dirname(__FILE__).'/../application/third_party/CIUnitTest/CIUnit.php'));
