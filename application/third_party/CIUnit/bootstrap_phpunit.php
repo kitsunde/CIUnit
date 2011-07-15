cc30d0821cc9c6f87d4649ccb2094b4d04014f9e
kenjis <none@none>
2011-07-15 13:55:12 +0900

    add ENVIRONMENT constant

--- application/third_party/CIUnit/bootstrap_phpunit.php
+++ application/third_party/CIUnit/bootstrap_phpunit.php
@@ -6,6 +6,26 @@ if ( ! defined('CIUnit_Version') ) {
 
 /*
  *---------------------------------------------------------------
+ * APPLICATION ENVIRONMENT
+ *---------------------------------------------------------------
+ *
+ * You can load different configurations depending on your
+ * current environment. Setting the environment also influences
+ * things like logging and error reporting.
+ *
+ * This can be set to anything, but default usage is:
+ *
+ *   development
+ *   testing
+ *   production
+ *
+ * NOTE: If you change these, also change the error_reporting() code below
+ *
+ */
+	define('ENVIRONMENT', 'testing');
+
+/*
+ *---------------------------------------------------------------
  * PHP ERROR REPORTING LEVEL
  *---------------------------------------------------------------
  *
