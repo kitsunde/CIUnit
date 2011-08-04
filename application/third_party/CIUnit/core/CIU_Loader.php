16ec672ccb451c48e8ac58c129627d6cafb2498d
kenjis <none@none>
2011-08-04 21:09:25 +0900

    update core/CIU_Loader.php

--- application/third_party/CIUnit/core/CIU_Loader.php
+++ application/third_party/CIUnit/core/CIU_Loader.php
@@ -1,4 +1,4 @@
-<?php
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
 /*
 * fooStack, CIUnit for CodeIgniter
@@ -20,23 +20,23 @@
 class CIU_Loader extends CI_Loader {
 
 	var $_ci_loaded_files = array();
-	
+
 	/**
 	 * Load class
 	 *
 	 * This function loads the requested class.
 	 *
-	 * @access  private
-	 * @param   string  the item that is being loaded
-	 * @param   mixed   any additional parameters
-	 * @return  void
+	 * @param	string	the item that is being loaded
+	 * @param	mixed	any additional parameters
+	 * @param	string	an optional object name
+	 * @return	void
 	 */
-	function _ci_load_class($class, $params = NULL, $object_name = NULL)
+	protected function _ci_load_class($class, $params = NULL, $object_name = NULL)
 	{
 		// Get the class name, and while we're at it trim any slashes.
 		// The directory path can be included as part of the class name,
 		// but we don't want a leading slash
-		$class = str_replace(EXT, '', trim($class, '/'));
+		$class = str_replace('.php', '', trim($class, '/'));
 
 		// Was the path included with the class name?
 		// We look for a slash to determine this
@@ -53,12 +53,12 @@ class CIU_Loader extends CI_Loader {
 		// We'll test for both lowercase and capitalized versions of the file name
 		foreach (array(ucfirst($class), strtolower($class)) as $class)
 		{
-			$subclass = APPPATH.'libraries/'.$subdir.config_item('subclass_prefix').$class.EXT;
+			$subclass = APPPATH.'libraries/'.$subdir.config_item('subclass_prefix').$class.'.php';
 
 			// Is this a class extension request?
 			if (file_exists($subclass))
 			{
-				$baseclass = BASEPATH.'libraries/'.ucfirst($class).EXT;
+				$baseclass = BASEPATH.'libraries/'.ucfirst($class).'.php';
 
 				if ( ! file_exists($baseclass))
 				{
@@ -66,11 +66,11 @@ class CIU_Loader extends CI_Loader {
 					show_error("Unable to load the requested class: ".$class);
 				}
 
-				// Safety:  Was the class already loaded by a previous call?
+				// Safety: Was the class already loaded by a previous call?
 				if (in_array($subclass, $this->_ci_loaded_files))
 				{
 					// Before we deem this to be a duplicate request, let's see
-					// if a custom object name is being supplied.  If so, we'll
+					// if a custom object name is being supplied. If so, we'll
 					// return a new instance of the object
 					if ( ! is_null($object_name))
 					{
@@ -97,19 +97,19 @@ class CIU_Loader extends CI_Loader {
 			$is_duplicate = FALSE;
 			foreach ($this->_ci_library_paths as $path)
 			{
-				$filepath = $path.'libraries/'.$subdir.$class.EXT;
+				$filepath = $path.'libraries/'.$subdir.$class.'.php';
 
-				// Does the file exist?  No?  Bummer...
+				// Does the file exist? No? Bummer...
 				if ( ! file_exists($filepath))
 				{
 					continue;
 				}
 
-				// Safety:  Was the class already loaded by a previous call?
+				// Safety: Was the class already loaded by a previous call?
 				if (in_array($filepath, $this->_ci_loaded_files))
 				{
 					// Before we deem this to be a duplicate request, let's see
-					// if a custom object name is being supplied.  If so, we'll
+					// if a custom object name is being supplied. If so, we'll
 					// return a new instance of the object
 					if ( ! is_null($object_name))
 					{
@@ -132,7 +132,7 @@ class CIU_Loader extends CI_Loader {
 
 		} // END FOREACH
 
-		// One last attempt.  Maybe the library is in a subdirectory, but it wasn't specified?
+		// One last attempt. Maybe the library is in a subdirectory, but it wasn't specified?
 		if ($subdir == '')
 		{
 			$path = strtolower($class).'/'.$class;
@@ -148,38 +148,88 @@ class CIU_Loader extends CI_Loader {
 		}
 	}
 
-	 /**
+	// --------------------------------------------------------------------
+
+	/**
 	 * Instantiates a class
 	 *
-	 * @access  private
-	 * @param   string
-	 * @param   string
-	 * @return  null
+	 * @param	string
+	 * @param	string
+	 * @param	string	an optional object name
+	 * @return	null
 	 */
-	function _ci_init_class($class, $prefix = '', $config = FALSE, $object_name = NULL)
+	protected function _ci_init_class($class, $prefix = '', $config = FALSE, $object_name = NULL)
 	{
-		// Is there an associated config file for this class?
+		// Is there an associated config file for this class? Note: these should always be lowercase
 		if ($config === NULL)
 		{
-			foreach(array(ucfirst($class), strtolower($class)) as $clsName) {
-				if (file_exists(APPPATH.'config/'.$clsName.EXT))
+			// Fetch the config paths containing any package paths
+			$config_component = $this->_ci_get_component('config');
+
+			if (is_array($config_component->_config_paths))
+			{
+				// Break on the first found file, thus package files
+				// are not overridden by default paths
+				foreach ($config_component->_config_paths as $path)
 				{
-					include(APPPATH.'config/'.$clsName.EXT);
+					// We test for both uppercase and lowercase, for servers that
+					// are case-sensitive with regard to file names. Check for environment
+					// first, global next
+					if (defined('ENVIRONMENT') AND file_exists($path .'config/'.ENVIRONMENT.'/'.strtolower($class).'.php'))
+					{
+						include_once($path .'config/'.ENVIRONMENT.'/'.strtolower($class).'.php');
+						break;
+					}
+					elseif (defined('ENVIRONMENT') AND file_exists($path .'config/'.ENVIRONMENT.'/'.ucfirst(strtolower($class)).'.php'))
+					{
+						include_once($path .'config/'.ENVIRONMENT.'/'.ucfirst(strtolower($class)).'.php');
+						break;
+					}
+					elseif (file_exists($path .'config/'.strtolower($class).'.php'))
+					{
+						include_once($path .'config/'.strtolower($class).'.php');
+						break;
+					}
+					elseif (file_exists($path .'config/'.ucfirst(strtolower($class)).'.php'))
+					{
+						include_once($path .'config/'.ucfirst(strtolower($class)).'.php');
+						break;
+					}
 				}
 			}
 		}
 
 		if ($prefix == '')
 		{
-			$name = (class_exists('CI_'.$class)) ? 'CI_'.$class : $class;
+			if (class_exists('CI_'.$class))
+			{
+				$name = 'CI_'.$class;
+			}
+			elseif (class_exists(config_item('subclass_prefix').$class))
+			{
+				$name = config_item('subclass_prefix').$class;
+			}
+			else
+			{
+				$name = $class;
+			}
 		}
 		else
 		{
 			$name = $prefix.$class;
 		}
 
+		// Is the class name valid?
+		if ( ! class_exists($name))
+		{
+			log_message('error', "Non-existent class: ".$name);
+			show_error("Non-existent class: ".$class);
+		}
+
 		// Set the variable name we will assign the class to
+		// Was a custom class name supplied? If so we'll use it
 		$class = strtolower($class);
+
 		if (is_null($object_name))
 		{
 			$classvar = ( ! isset($this->_ci_varmap[$class])) ? $class : $this->_ci_varmap[$class];
@@ -188,15 +238,18 @@ class CIU_Loader extends CI_Loader {
 		{
 			$classvar = $object_name;
 		}
+
+		// Save the class name and object name
+		$this->_ci_classes[$class] = $classvar;
 		// Instantiate the class
 		$CI =& get_instance();
 		if ($config !== NULL)
 		{
-			if (!defined('CIUnit_Version'))
+			if ( ! defined('CIUnit_Version'))
 			{
 				$CI->$classvar = new $name($config);
 			}
-			elseif (!isset($CI->$classvar))
+			elseif ( ! isset($CI->$classvar))
 			{
 				//redesignme: check if we have got one already..
 				$CI->$classvar = new $name($config);
@@ -204,22 +257,20 @@ class CIU_Loader extends CI_Loader {
 		}
 		else
 		{
-			if (!defined('CIUnit_Version'))
+			if ( ! defined('CIUnit_Version'))
 			{
 				$CI->$classvar = new $name;
 			}
-			elseif (!isset($CI->$classvar))
+			elseif ( ! isset($CI->$classvar))
 			{
 				//redesignme: check if we have got one already..
 				$CI->$classvar = new $name($config);
 			}
 		}
-		
-		$this->_ci_classes[$class] = $classvar;
 	}
-	
+
 	// --------------------------------------------------------------------
-	
+
 	/**
 	 * Autoloader
 	 *
