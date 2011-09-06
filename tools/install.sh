61d917bf9136c480f49147debff0aa9f2a5bb29e
kenjis <none@none>
2011-09-07 08:54:12 +0900

    update install shell script
    --HG--
    extra : transplant_source : %AB%FC3%23x%9E%F2%1C%EBy%D9%A1%AE%A5%FE%1D%10z%0B%DF

--- tools/install.sh
+++ tools/install.sh
@@ -4,12 +4,18 @@
 # 2011/09/05  Kenji Suzuki
 
 if [ $# -eq 0 ]; then
-    echo " usage: $0 CI_Project_Path"
+    echo " usage: $0 CI_Project_Path [DB_Name [DB_User [DB_Passwd [DB_host]]]]"
     exit;
 fi
 
-cwd=`pwd`
-ci_path="$cwd/$1"
+ci_path="$1"
+check=`echo "$ci_path" | cut -c 1`
+
+if [ "$check" != "/" ]; then
+    cwd=`pwd`
+    ci_path="$cwd/$1"
+fi
+
 cd `dirname $0`
 
 # install CIUnit
@@ -42,8 +48,33 @@ fi
 mkdir -p application/config/testing
 cp application/config/database.php application/config/testing/
 cd application/config/testing
-sed -e "s/^\$db\['default'\]\['database'\] = '';/\$db['default']['database'] = 'ciunit_test';/" database.php > database.php.$$
+
+if [ $# -ge 2 ]; then
+    db_name="$2"
+else
+    db_name="ciunit_test"
+fi
+
+sed -e "s/^\$db\['default'\]\['database'\] = '';/\$db['default']['database'] = '$db_name';/" database.php > database.php.$$
 mv database.php.$$ database.php
+
+if [ $# -ge 3 ]; then
+    db_user="$3"
+    sed -e "s/^\$db\['default'\]\['username'\] = '';/\$db['default']['username'] = '$db_user';/" database.php > database.php.$$
+    mv database.php.$$ database.php
+fi
+
+if [ $# -ge 4 ]; then
+    db_passwd="$4"
+    sed -e "s/^\$db\['default'\]\['password'\] = '';/\$db['default']['password'] = '$db_passwd';/" database.php > database.php.$$
+    mv database.php.$$ database.php
+fi
+
+if [ $# -ge 5 ]; then
+    db_host="$5"
+    sed -e "s/^\$db\['default'\]\['hostname'\] = 'localhost';/\$db['default']['hostname'] = '$db_host';/" database.php > database.php.$$
+    mv database.php.$$ database.php
+fi
 
 cwd=`pwd`
 echo "$cwd/database.php created"
