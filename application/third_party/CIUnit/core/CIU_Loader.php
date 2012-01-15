6fe2944dd8302e7338ffb319ee575497b68425b3
kenjis <none@none>
2012-01-15 14:34:34 +0900

    fix #9, output() can't get controller output in PHPUnit 3.6.3 or later
    --HG--
    extra : transplant_source : %14%FB%08%92%DB%A0%9C/%BF%0F%98%7E%A1%0C%D1%E5%B5%C8%BE%B5

--- application/third_party/CIUnit/core/CIU_Loader.php
+++ application/third_party/CIUnit/core/CIU_Loader.php
@@ -403,6 +403,36 @@ class CIU_Loader extends CI_Loader {
 	// --------------------------------------------------------------------
 
 	/**
+	* Load View
+	*
+	* This function is used to load a "view" file.  It has three parameters:
+	*
+	* 1. The name of the "view" file to be included.
+	* 2. An associative array of data to be extracted for use in the view.
+	* 3. TRUE/FALSE - whether to return the data or load it.  In
+	* some cases it's advantageous to be able to return data so that
+	* a developer can process it in some way.
+	*
+	* @param	string
+	* @param	array
+	* @param	bool
+	* @return	void
+	*/
+	public function view($view, $vars = array(), $return = FALSE)
+	{
+		if ($return === TRUE)
+		{
+			return parent::view($view, $vars, $return);
+		}
+
+		$output = $this->_ci_load(array('_ci_view' => $view, '_ci_vars' => $this->_ci_object_to_array($vars), '_ci_return' => TRUE));
+		$CI =& get_instance();
+		$CI->output->add_output($output);
+	}
+
+	// --------------------------------------------------------------------
+
+	/**
 	 * Load Helper
 	 *
 	 * This function loads the specified helper file.
